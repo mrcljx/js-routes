ed45a1b792656a434dba28480e0204a18b817ad2
Marcel Jackwerth <marcel@northdocks.com>
2011-02-23 01:40:33 +0100

    return array instead of string (should work with ruby 1.9 now)

--- lib/jsroutes/rack.rb
+++ lib/jsroutes/rack.rb
@@ -3,11 +3,11 @@ module JSRoutes
     def initialize(app)
       @app = app
     end
-    
+
     def call(env)
       if env['PATH_INFO'] =~ /^#{Regexp.escape(JSRoutes.mount_url)}$/
         script = JSRoutes.build
-        [200, {'Content-Type' => 'text/javascript', 'Content-Length' => script.length.to_s}, script]
+        [200, {'Content-Type' => 'text/javascript', 'Content-Length' => script.length.to_s}, [script]]
       else
         @app.call(env)
       end
